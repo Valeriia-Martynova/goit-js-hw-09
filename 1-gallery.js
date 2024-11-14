34ff72b6260c32a70728d58705dd4f97cd5e1f40
Valeriia-Martynova <vmartynova95@gmail.com>
2024-11-14 19:03:29 +0000

    Deploying to gh-pages from @ Valeriia-Martynova/goit-js-hw-09@96e40d1522385974564fff39e9619666419108bf 🚀

---
+++ 1-gallery.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{S as o}from"./assets/vendor-CgTBfC_f.js";new o(".some-element a",{});console.log(o);
+//# sourceMappingURL=1-gallery.js.map
