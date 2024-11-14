96e40d1522385974564fff39e9619666419108bf
Valeriia Martynova <vmartynova95@gmail.com>
2024-11-14 21:01:25 +0200

    update files

---
+++ src/js/1-gallery.js
@@ -0,0 +1,10 @@
+
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+const Lightbox = new SimpleLightbox('.some-element a', { /* options */ });
+
+
+console.log(SimpleLightbox);
